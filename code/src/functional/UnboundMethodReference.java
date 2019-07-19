adfaf91402bf8eae0028575a1ca169c2cba715c9
Levid_GC <levid.gc@outlook.com>
2019-07-19 21:51:15 +0800

    update

---
+++ code/src/functional/UnboundMethodReference.java
@@ -0,0 +1,29 @@
+// functional/UnboundMethodReference.java
+
+package functional;
+
+class X {
+    String f() {
+        return "X::f()";
+    }
+}
+
+interface MakeString {
+    String make();
+}
+
+interface TransformX {
+    String transform(X x);
+}
+
+/**
+ * UnboundMethodReference
+ */
+public class UnboundMethodReference {
+    public static void main(String[] args) {
+        TransformX sp = X::f;
+        X x = new X();
+        System.out.println(sp.transform(x));
+        System.out.println(x.f());
+    }
+}
